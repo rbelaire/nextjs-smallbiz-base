2349326f75bc729e6830f26d0d2cceb255776954
Ryan Belaire <rjbelaire@gmail.com>
2026-02-11 12:18:28 -0600

    Add custom Tailwind theme tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         accent: "#f79ac0",
         neutral: "#f1d3b2",
       },
+      fontSize: {
+        h1: ["3rem", { lineHeight: "1.1", fontWeight: "700" }],
+        h2: ["2rem", { lineHeight: "1.2", fontWeight: "700" }],
+        body: ["1rem", { lineHeight: "1.6" }],
+        small: ["0.875rem", { lineHeight: "1.5" }],
+      },
+      spacing: {
+        sectionVertical: "6rem",
+        containerPadding: "1.5rem",
+      },
     },
   },
   plugins: [],
